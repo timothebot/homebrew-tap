0b753411978c95595e31c6578a851e45eb44c06e
tiimo <65387160+timothebot@users.noreply.github.com>
2025-09-07 18:24:20 +0200

    fix: File hashes

--- Formula/lacy.rb
+++ Formula/lacy.rb
@@ -6,24 +6,24 @@ class Lacy < Formula
   on_macos do
     on_intel do
       url "https://github.com/timothebot/lacy/releases/download/v0.4.0/lacy-lacy-x86_64-apple-darwin"
-      sha256 "569ce0081cb5b9c025f747ebcf5b4979cd0e8c5195fc9b0d02c5f770b1616fee"
+      sha256 "600d3dd53968fe628140cbb095945e36c82ada8a4ed8258adb162d937123f8f7"
     end
 
     on_arm do
       url "https://github.com/timothebot/lacy/releases/download/v0.4.0/lacy-lacy-aarch64-apple-darwin"
-      sha256 "d37f61e6176fc2f7e7f12cd7592aafbaf22656dfc17f1cb0f214a2e7da2fd452"
+      sha256 "df3606f41d90125172710fb0c39fc78af1226404ce2dc30dbd519e9a466a9e63"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/timothebot/lacy/releases/download/v0.4.0/lacy-lacy-x86_64-unknown-linux-gnu"
-      sha256 "bf54fd810f71cb492206445fb0a67ddbf0a9814a607b45f8422b4400350486eb"
+      sha256 "4351eeb2e090ee5dc488e7e87d0fd3296471c1234450d1df97069fc939a1df6c"
     end
 
     on_arm do
       url "https://github.com/timothebot/lacy/releases/download/v0.4.0/lacy-lacy-aarch64-unknown-linux-gnu"
-      sha256 "fc84de12f72e0831434ae659ca1e341148ab5b1e95e11ab39ec65154a408e614"
+      sha256 "13b9a312e94500810c57283ba08cd24da9babfc729387980c3109721bedb66b1"
     end
   end
 
