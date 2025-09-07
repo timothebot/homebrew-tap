84c6277c033347f8087884ab30121b20754a903f
tiimo <65387160+timothebot@users.noreply.github.com>
2025-09-07 09:47:12 +0200

    fox: Add missing newline

--- Formula/lacy.rb
+++ Formula/lacy.rb
@@ -8,7 +8,7 @@ class Lacy < Formula
       url "https://github.com/timothebot/lacy/releases/download/v0.2.2/lacy-lacy-x86_64-apple-darwin"
       sha256 "77295ce24714a843e40cd9a082e5139f536f9bc2d13ed7bace2e4c25e9a7a21b"
     end
-    
+
     on_arm do
       url "https://github.com/timothebot/lacy/releases/download/v0.2.2/lacy-lacy-aarch64-apple-darwin"
       sha256 "58a61597d8dfa681a9dc4f099e9c8d02fb8de925f7e17ca13ff40a0aa23e75db"
